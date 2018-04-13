91f6bd6f6f1da6f689d677d3a31c8c5e3caff277
Mahima Manik <mcs172093@iitd.ac.in>
2018-04-14 00:09:19 +0530

    Stake_holder done , money added successful to all

--- Assignment2/hello_world_voting/Media.sol
+++ Assignment2/hello_world_voting/Media.sol
@@ -8,7 +8,12 @@ byteCode = compiledCode.contracts[':Media'].bytecode
 deployedContract = VotingContract.new((web3.eth.accounts), {data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
 contractInstance = VotingContract.at(deployedContract.address)
 contractInstance.validateCreator(web3.eth.accounts[0], {from: web3.eth.accounts[0]})
- */
+web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0])).toString()
+web3.fromWei(web3.eth.getBalance(web3.eth.accounts[1])).toString()
+web3.fromWei(web3.eth.getBalance(web3.eth.accounts[2])).toString()
+web3.fromWei(web3.eth.getBalance(web3.eth.accounts[3])).toString()
+web3.fromWei(web3.eth.getBalance(web3.eth.accounts[4])).toString()
+*/
 
 pragma solidity ^0.4.18;
 
@@ -18,18 +23,20 @@ contract Media 		{
 	address[] public creator_list;
 
 	struct media_c	{
+		
 		uint media_ID;
 		bytes32 creator;
 		address creator_add;
 		bytes32 url;
-		uint cost_for_individual;
-		uint cost_for_company;
+		uint256 cost_for_individual;
+		uint256 cost_for_company;
+		address[] stake_holder_list;
+		uint[] stake_list;
 	}
 
 	media_c[] media_list;
 	mapping (address=>uint) public type_customer;
 	
-	//deployedContract = VotingContract.new((['0x40a720901d4e971a8947132c6f15f84f69410b15', '0xc749aef1f0dc36ec660ab80280e799c69c07d8e1']), {data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
 	function Media (address[] cr_add) public  	{
 		for (uint j = 0; j < cr_add.length; j++)	{
 			if (j % 3 == 0)	{
@@ -55,10 +62,17 @@ contract Media 		{
 		return false;
 	}
 
-	//contractInstance.addMedia(2, 'Mahima', web3.eth.accounts[0], 'https://www.mygaana.com', 12, 32, {from: web3.eth.accounts[0], gas:4700000})
-	//contractInstance.addMedia(3, 'Khushboo', web3.eth.accounts[1], 'https://www.mygaana.com', 12, 32, {from: web3.eth.accounts[0], gas:4700000})
-	function addMedia(uint ID, bytes32 cr, address cr_add, bytes32 url, uint c_i, uint c_c) public {
-		media_list.push(media_c(ID, cr, cr_add, url, c_i, c_c));
+/*
+contractInstance.addMedia(1, 'Mahima', web3.eth.accounts[0], 'https://www.mygaana.com', web3.toWei('0.01'), web3.toWei('0.02'),[web3.eth.accounts[1],web3.eth.accounts[2],web3.eth.accounts[3]],[20,20,20], {from: web3.eth.accounts[0], gas:4700000})
+contractInstance.addMedia(2, 'Khushboo', web3.eth.accounts[3], 'https://www.mygaana.com', web3.toWei('0.01'), web3.toWei('0.02'),[web3.eth.accounts[3],web3.eth.accounts[4],web3.eth.accounts[5]],[30,30,20], {from: web3.eth.accounts[3], gas:4700000})
+*/
+	function addMedia(uint ID, bytes32 cr, address cr_add, bytes32 url, uint256 c_i, uint256 c_c, address[] stake_hol_list, uint[] stake_per_list) public {
+		require(validateCreator(cr_add));
+		require(stake_hol_list.length <= 5);
+		require(stake_hol_list.length == stake_per_list.length);
+		media_list.push(media_c(ID, cr, cr_add, url, c_i, c_c,stake_hol_list,stake_per_list));
+		
+		
 	}
 
 	//contractInstance.countMedia({from: web3.eth.accounts[0]}).toString()
@@ -77,17 +91,45 @@ contract Media 		{
 		return (temp, temp2);
 	}
 
-	function buyMedia(address recv_add, uint mID) public payable {
+	function validateAmount(uint cost, uint sent_am) view public returns (bool)		{
+		if ( cost == sent_am )	{
+			return true;
+		}
+		return false;
+	}
+
+	//Parameters : buyer address, mediaID
+/*
+contractInstance.buyMedia(web3.eth.accounts[5], 1, {from: web3.eth.accounts[5],value: web3.toWei('0.01','ether') , gas:4700000})
+*/
+	function buyMedia(address recv_add, uint mID) public payable{
 		uint temp = type_customer[recv_add];
-		for (uint i=0; i<media_list.length;i++){
-			if (media_list[i].media_ID == mID){
-				if temp ==	0		{
-					uint cost = media_list[i].cost_for_company;
+		address r_addr;
+		uint256 cost;
+		uint index_media ; 
+		for (uint i = 0; i < media_list.length; i++){
+			
+			if (media_list[i].media_ID == mID) {
+				index_media = i;
+				if ( temp == 0 )	{
+					cost = media_list[i].cost_for_company;
+					r_addr = media_list[i].creator_add;
 				}
 				else {
-					uint cost = media_list[i].cost_for_individual;
+					cost = media_list[i].cost_for_individual;
+					r_addr = media_list[i].creator_add;
 				}
 			}
 		}
+		require(validateAmount(cost,msg.value));
+		uint stake_share = 0;
+		for (uint s_addr = 0; s_addr < media_list[index_media].stake_holder_list.length;s_addr++){
+			media_list[index_media].stake_holder_list[s_addr].transfer(msg.value*media_list[index_media].stake_list[s_addr]/100);
+			stake_share += media_list[index_media].stake_list[s_addr];
+		}
+		r_addr.transfer(msg.value*(100 - stake_share)/100);
 	}
+
+	
+
 }
